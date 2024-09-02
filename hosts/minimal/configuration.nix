7bcaedee7de73a65715d47c951c91479e647e572
Atolycs <111569596+atolycs@users.noreply.github.com>
2024-09-02 03:18:14 +0000

    feat: ld config

--- hosts/minimal/configuration.nix
+++ hosts/minimal/configuration.nix
@@ -127,6 +127,13 @@
     };
   };
 
+  program = {
+    nix-ld = {
+      enable = true;
+      libraries = with pkgs; [
+      ];
+    };
+  };
   # This option defines the first version of NixOS you have installed on this particular machine,
   # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
   #
