e567a1bcf9b3d21d1569bff97783c1024190ac95
Atolycs <111569596+atolycs@users.noreply.github.com>
2024-09-26 14:39:49 +0900

    chore: default font config

--- modules/common/config/fonts.nix
+++ modules/common/config/fonts.nix
@@ -9,5 +9,13 @@
      noto-fonts-monochrome-emoji
      nerdfonts
     ];
+    fontconfig = {
+     defaultFonts = {
+       serif = ["Noto Serif CJK JP" "Noto Color Emoji"];
+       sansSerif = ["Noto Sans CJK JP" "NotoColor Emoji"];
+       monospace = ["JetBrainsMono Nerd Font" "Noto Color Emoji"];
+       emoji = ["Noto Color Emoji"];
+     };
+    };
   };
 }
