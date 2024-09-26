c78d68c808e087d6b0e5f957dde596bbd52a028b
Atolycs <111569596+atolycs@users.noreply.github.com>
2024-09-26 23:48:26 +0900

    delete: nerdfonts package

--- modules/common/config/fonts.nix
+++ modules/common/config/fonts.nix
@@ -7,13 +7,11 @@
      noto-fonts-cjk-serif
      noto-fonts-color-emoji
      noto-fonts-monochrome-emoji
-     nerdfonts
     ];
     fontconfig = {
      defaultFonts = {
        serif = ["Noto Serif CJK JP" "Noto Color Emoji"];
        sansSerif = ["Noto Sans CJK JP" "NotoColor Emoji"];
-       monospace = ["JetBrainsMono Nerd Font" "Noto Color Emoji"];
        emoji = ["Noto Color Emoji"];
      };
     };
