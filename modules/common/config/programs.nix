59e364914c4218c6f2e6240dbba75fc56a16ab05
Atolycs <111569596+atolycs@users.noreply.github.com>
2024-09-26 14:36:32 +0900

    feat: firefox extension add

--- modules/common/config/programs.nix
+++ modules/common/config/programs.nix
@@ -6,6 +6,14 @@
     firefox = {
       enable = true;
       package = pkgs.firefox-esr;
+      policies = {
+       ExtensionSettings = {
+        "uBlock0@raymondhill.net" = {
+	  install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
+	  installation_mode = "force_installed";
+	};
+       };
+      };
     };
 
     gnupg.agent = {
