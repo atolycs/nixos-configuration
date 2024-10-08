a17d68f89da3c8682a8effb326d3d7cc769fc99f
Atolycs <111569596+atolycs@users.noreply.github.com>
2024-10-08 18:55:59 +0900

    feat: font add

--- modules/config-set/desktop/gdm/fonts.nix
+++ modules/config-set/desktop/gdm/fonts.nix
@@ -4,9 +4,7 @@
     packages = with pkgs; [
       noto-fonts-cjk-serif
       noto-fonts-emoji-serif
-      "${pkgs.fetchzip {
-          url = "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/0xProto.zip";
-      }}"
+      (nerdfonts.override { fonts = [ "0xProto" ]; })
     ];
     fontDir.enable = true;
 
