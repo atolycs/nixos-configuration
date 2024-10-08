83f8df22e6f59347f889b4f2b9341c7bbbcad718
Atolycs <111569596+atolycs@users.noreply.github.com>
2024-10-08 16:48:35 +0900

    feat: extension enable

--- home-manager/desktop/gdm/dconf.nix
+++ home-manager/desktop/gdm/dconf.nix
@@ -1,24 +1,17 @@
 {
-  programs = {
-    dconf = {
-     enable = true;
-     profiles.user.databases = [
-        {
-          settings = {
-            "org/gnome/desktop/wm/preferences" = {
-              button-layout = "appmenu:minimize,maximize,close";
-            };
-
-            "org/gnome/shell" = {
-              disable-user-extensions = false;
-              enabled-extensions = [
-                "kimpanel@kde.org"
-                "user-theme@gnome-shell-extensions.gcampax.github.com"
-              ];
-            };
-          };
-        }
-     ];
+  dconf = {
+   enable = true;
+   settings = {
+    "org/gnome/desktop/wm/preferences" = {
+      button-layout = "appmenu:minimize,maximize,close";
+    };
+    "org/gnome/shell" = {
+      disable-user-extensions = false;
+      enabled-extensions = [
+        "kimpanel@kde.org"
+         "user-theme@gnome-shell-extensions.gcampax.github.com"
+      ];
     };
   };
+ };
 }
