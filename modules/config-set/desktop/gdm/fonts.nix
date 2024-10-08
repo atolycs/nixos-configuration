abdea2a43b8bf532456d789583fc7f684a225157
Atolycs <111569596+atolycs@users.noreply.github.com>
2024-10-08 19:05:02 +0900

    feat: add nerdfont

--- modules/config-set/desktop/gdm/fonts.nix
+++ modules/config-set/desktop/gdm/fonts.nix
@@ -4,9 +4,18 @@
     packages = with pkgs; [
       noto-fonts-cjk-serif
       noto-fonts-emoji-serif
-      (nerdfonts.override { fonts = [ "0xProto" ]; })
+      (nerdfonts.override { fonts = [ "0xProto" "JetBrainsMono" ]; })
     ];
     fontDir.enable = true;
 
+    fontconfig = {
+      defaultFonts = {
+        serif = [ "Noto Serif CJK JP" "Noto Color Emoji" ];
+        sansSerif = [ "Noto Sans CJK JP" "Noto Color Emoji" ];
+        monospace = [ "JetBrainsMono Nerd Font" "Noto Color Emoji" ];
+        emoji = [ "Noto Color Emoji" ];
+      };
+    };
   };
 }
+
