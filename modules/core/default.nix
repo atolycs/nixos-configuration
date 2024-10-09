0ab88948ad68b0ea5f090f340a7b2194806781ad
Atolycs <111569596+atolycs@users.noreply.github.com>
2024-10-09 12:01:03 +0900

    feat: add tmux

--- modules/core/default.nix
+++ modules/core/default.nix
@@ -1,11 +1,12 @@
 {
   imports = [
-   ./config/boot.nix
-   ./config/i18n.nix
-   ./config/time.nix
-   ./config/nix.nix
-   ./config/sound.nix
-   ./config/security.nix
-   ./config/ssh.nix
+    ./config/boot.nix
+    ./config/i18n.nix
+    ./config/time.nix
+    ./config/nix.nix
+    ./config/sound.nix
+    ./config/security.nix
+    ./config/ssh.nix
+    ./config/tmux.nix
   ];
 }
