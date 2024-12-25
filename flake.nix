0b5632dbb5d4d96b590e5f46e493348d74aee1e8
Atolycs <111569596+atolycs@users.noreply.github.com>
2024-12-25 10:37:16 +0900

    feat: add flake.nix shell.nix

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "Atolycs nixos configuration";
+
+  inputs = {
+    nixpkgs-unstable.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    home-manager = {
+      url = "github:nixo-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    flake-utils = { url = "github:numtide/flake-utils"; };
+  };
+
+  outputs =
+    { self, nixpkgs, nixpkgs-unstable, home-manager, flake-utils, ... }@inputs:
+    let
+      stateVersion = "24.11";
+      inherit (sefl) outputs;
+      inherit (nixpkgs.lib.lists) remove;
+      inherit (nixpkgs.lib) replacestrings genAttrs;
+      inherit (nixpkgs.lib.path) removePrefix splitRoot;
+      inherit (nixpkgs.lib.filessytem) listFilesRecursive;
+
+      # nix-helper = import ./lib {
+      #    inherit outputs self stateVersion;
+      #};
+    in flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in { devShells.default = import ./shell.nix { inherit pkgs; }; });
+}
