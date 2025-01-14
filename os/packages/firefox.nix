8d0c183d9e1ccf5bfb55268bab8bf8264f1ba35a
Atolycs <111569596+atolycs@users.noreply.github.com>
2025-01-14 11:27:24 +0900

    feat: add packages

---
+++ os/packages/firefox.nix
@@ -0,0 +1,20 @@
+{ pkgs, ...}:
+{
+  programs = {
+      firefox = {
+          enable = true;
+          package = pkgs.firefox-esr;
+          policies = {
+                      ExtensionSettings = {
+          "uBlock0@raymondhill.net" = {
+            install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
+            installation_mode = "normal_installed";
+          };
+          "@testpilot-containers" = {
+            install_url = "https://addons.mozilla.org/firefox/downloads/latest/multi-account-containers/latest.xpi";
+            installation_mode = "normal_installed";
+          };
+            }
+        }
+    }
+}
---
+++ os/packages/tmux.nix
@@ -0,0 +1,9 @@
+{
+    programs.tmux = {
+        enable = true;
+        clock24 = true;
+        extraConfig = ''
+          set-window-option -g mode-keys vi
+        '';
+      };
+  }
