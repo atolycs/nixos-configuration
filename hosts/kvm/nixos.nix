f6afb5c52e298eb7abe1e1de7d505760d06ac8bb
Atolycs <111569596+atolycs@users.noreply.github.com>
2025-01-14 18:13:35 +0900

    feat: add kvm

---
+++ hosts/kvm/nixos.nix
@@ -0,0 +1,40 @@
+{
+  hostname,
+  stateSystem,
+  lib,
+  config,
+  modulesPath,
+  outputs,
+  ...
+}:
+{
+  imports = [
+    outputs.nixosModules.host-hardware.kvm
+    outputs.nixosModules.nix
+    outputs.nixosModules.boot.systemd
+
+    ./mountPoint
+    ../../os/boot
+    ../../os/security
+    ../../os/locale
+    ../../os/systemd/systemd.nix
+  ];
+
+  networking = {
+    hostName = "${hostname}";
+  };
+
+  nixpkgs.hostPlatform = "x86_64-linux";
+
+  boot = {
+    initrd = {
+      availableKernelModules = [
+        "nvme"
+        "xhci_pci"
+        "sd_mod"
+      ];
+    };
+  };
+
+  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+}
