58df38b9141090c1931883ec5300eda0cdd0d622
Atolycs <111569596+atolycs@users.noreply.github.com>
2025-01-14 09:43:09 +0900

    feat: add hostPlatform

--- hosts/vmware/nixos.nix
+++ hosts/vmware/nixos.nix
@@ -5,12 +5,11 @@
     ...
 }: {
     imports = [
-      ./mountPoint/
+      ./mountPoint
     ];
 
     network = {
         hostName = ${hostname};
     };
 
-    nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
 }
