e752fa271044807f0162fcd2a49cee1d0529c185
Atolycs <111569596+atolycs@users.noreply.github.com>
2025-01-19 13:01:08 +0900

    feat: devShell DIRTRIM environ add

--- shell.nix
+++ shell.nix
@@ -41,6 +41,7 @@ pkgs.stdenv.mkDerivation {
 
   shellHook = ''
     source ${pkgs.git}/share/bash-completion/completions/git-prompt.sh;
+    export PROMPT_DIRTRIM=2
     export PS1='\n\[\033[1;32m\][devShell is \[\033[0;33m\]$(echo $name)\[\033[1;32m\]:\w]$(__git_ps1 "(%s)")\$\[\033[0m\] '
   '';
 
