aae72c214e8bbb3d2392400b8c74d4412ca8d5bd
Atolycs <111569596+atolycs@users.noreply.github.com>
2025-01-21 13:12:07 +0900

    feat: update systemd-boot modules

--- hosts/nahida/mountPoint/boot.nix
+++ hosts/nahida/mountPoint/boot.nix
@@ -1,5 +1,8 @@
-{ ... }:
+{ outputs, ... }:
 {
+  imports = [
+    outputs.nixosModules.boot.systemd
+  ];
   fileSystems."/boot" = {
     device = "/dev/disk/by-label/NIXOS-BOOT";
     fsType = "vfat";
