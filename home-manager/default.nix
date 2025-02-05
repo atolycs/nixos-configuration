96c8229b71ee65f9850d5639f51dcf77df40a8c2
Atolycs <111569596+atolycs@users.noreply.github.com>
2025-02-05 15:36:20 +0900

    fix: fcitx5

--- home-manager/default.nix
+++ home-manager/default.nix
@@ -3,14 +3,26 @@
   outputs,
   homeProfile,
   stateVersion,
+  pkgs,
   ...
 }:
 {
   imports = [
     ./${homeProfile}/home.nix
   ];
+  i18n.inputMethod = {
+      enabled = "fcitx5";
+      fcitx5 = {
+       addons = with pkgs; [fcitx5-mozc fcitx5-gtk];
+      };
+  };
 
   home = {
     inherit stateVersion;
+    # file = {
+    #   ".config/fcitx5/profile" = {
+    #     source = ./common/fcitx5/profile;
+    #   };
+    # };
   };
 }
