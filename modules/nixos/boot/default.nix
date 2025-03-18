e9affb0f2d43ca604892b4ca0ffbaab251698b81
Atolycs <111569596+atolycs@users.noreply.github.com>
2025-03-18 09:21:56 +0900

    feat: update dynamic load

--- modules/nixos/boot/default.nix
+++ modules/nixos/boot/default.nix
@@ -1,5 +1,14 @@
 # Bootloader modules
-{
-  systemd = import ./systemd.nix;
-  grub = import ./grub.nix;
-}
+
+let
+  moduleDirs = builtins.filter (x: x != "default.nix") ( builtins.attrNames (builtins.readDir ./.));
+
+  dynamicAttrs = builtins.listToAttrs(
+   map ( dir: {
+    name = builtins.replaceStrings[ ".nix" ] [ "" ] (builtins.baseNameOf dir);
+    value = import ././${dir};
+   }) moduleDirs
+  );
+in
+
+dynamicAttrs
