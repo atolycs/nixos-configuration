675d5295c2928e59d62f03c0f36ce397bd0071be
Atolycs <111569596+atolycs@users.noreply.github.com>
2025-03-18 09:35:16 +0900

    feat: dynamic load

--- modules/nixos/programs/default.nix
+++ modules/nixos/programs/default.nix
@@ -1,7 +1,21 @@
-{
-  git = import ./git;
-  firefox-esr = import ./firefox-esr;
-  vim = import ./vim;
+let
+  moduleDirs = builtins.filter(x: x != "default.nix") (builtins.attrNames (builtins.readDir ./.));
 
-  cloudflared = import ./cloudflared;
-}
+  dynamicAttrs = builtins.listToAttrs(
+   map (dir: {
+     name = builtins.baseNameOf dir;
+     value = import ././${dir};
+   }) moduleDirs
+  );
+in
+
+dynamicAttrs 
+
+
+# {
+#   git = import ./git;
+#   firefox-esr = import ./firefox-esr;
+#   vim = import ./vim;
+#
+#   cloudflared = import ./cloudflared;
+# }
