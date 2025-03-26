43e0c7f608b77a48d435c1e2ee562685c93efc7e
Atolycs <111569596+atolycs@users.noreply.github.com>
2025-03-26 15:06:46 +0900

    feat: base code

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "Atolycs's NixOS configuration";
+
+  outputs = inputs @ {nixpkgs, ...}:
+  let
+    
+  in 
+
+  {
+
+  };
+
+  inputs = {
+    nixpkgs = {
+     url = "github:nixos/nixpkgs/nixos-24.11";
+    };
+    nixpkgs-unstable = {
+     url = "github:nixos/nixpkgs/nixos-unstable";
+    };
+  };
+}
