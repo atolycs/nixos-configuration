fe1f3a082976fe9645f3b499902a318f48544ff2
Atolycs <111569596+atolycs@users.noreply.github.com>
2025-03-27 15:20:46 +0900

    feat: add user module

--- config/default.nix
+++ config/default.nix
@@ -1,4 +1,17 @@
-{ config,...}:let
-  
+{ config,...}:
+with lib;
+let
+  cfg = config.atlcfg;
    
 in 
+{
+  options = {
+    atlcfg = {
+      user = mkOption {
+        default = "atolycs";
+        type = types.str;
+        description = "Set add UserName";
+      };
+    };
+  };
+}
