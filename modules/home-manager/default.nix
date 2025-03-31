674b74a61aff3971bd0f83d0f77e6495f09db1f1
Atolycs <111569596+atolycs@users.noreply.github.com>
2025-03-31 10:10:36 +0900

    feat: add modules dir

---
+++ modules/home-manager/default.nix
@@ -0,0 +1,9 @@
+let
+   hmModuleDirs = builtins.filter(x: x != "default.nix") (builtins.attrNames (builtins.readDir ./.));
+   dynamicAttrs = builtins.listToAttrs (
+     map (dir : {
+        name = builtins.baseNameOf dir;
+        value = import ././${dir};
+      }) hmModuleDirs
+   );
+in  dynamicAttrs
---
+++ modules/nixos/default.nix
@@ -0,0 +1,9 @@
+let
+   nixosModuleDirs = builtins.filter(x: x != "default.nix") (builtins.attrNames (builtins.readDir ./.));
+   dynamicAttrs = builtins.listToAttrs (
+     map (dir : {
+        name = builtins.baseNameOf dir;
+        value = import ././${dir};
+      }) nixosModuleDirs
+   );
+in  dynamicAttrs
