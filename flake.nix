58e4c8528b6bb80f93cf11ad4d80612cc08171b8
Atolycs <111569596+atolycs@users.noreply.github.com>
2025-04-03 14:57:24 +0900

    feat: add flake

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "Atolycs NixOS configuration";
+
+  inputs = {
+    nixpkgs-unstable = {
+      url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    };
+    nixpkgs = {
+      url = "github:nixos/nixpkgs/nixos-24.11";
+    };
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+    };
+  };
+}
