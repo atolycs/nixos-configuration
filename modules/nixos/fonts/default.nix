4525d6e0c51db6d533fb34721535dd884c1f075c
Atolycs <111569596+atolycs@users.noreply.github.com>
2025-04-07 18:59:46 +0900

    feat: add fonts

---
+++ modules/nixos/fonts/default.nix
@@ -0,0 +1,39 @@
+{
+  pkgs,
+  ...
+}: {
+  config = {
+    fonts = {
+      packages = with pkgs;
+      [
+        noto-fonts
+        noto-fonts-cjk-sans
+        noto-fonts-emoji
+        (nerdfonts.override { fonts = ["FiraCode" "DroidSansMono"];})
+      ];
+      
+      fontDir.enable = true;
+
+      fontconfig = {
+        defaultFonts = {
+          serif = [
+            "Noto Serif CJK JP"
+            "Noto Color Emoji"
+          ];
+
+          sansSerif = [
+            "Noto Sans CJK JP"
+            "Noto Color Emoji"
+          ];
+
+          monospace = [
+            "JetBrainsMono Nerd Font"
+            "Noto Color Emoji"
+          ];
+
+          emoji = [ "Noto Color Emoji" ];
+        };
+      };
+    };
+  };
+}
