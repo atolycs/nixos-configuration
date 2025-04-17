d1a522cfe63eb065ca0c2ebe4c12240947343d17
Atolycs <111569596+atolycs@users.noreply.github.com>
2025-04-17 09:17:46 +0900

    feat: add home-manager loader

---
+++ lib/mkHome.nix
@@ -0,0 +1,35 @@
+# cLibs args
+args@{ self, ... }:
+# function args
+# https://discourse.nixos.org/t/import-from-files-dirs/36372/3
+{ homeProfile, config }:
+with args;
+inputs.home-manager.lib.homeManageConfiguration {
+   extraSpecialArgs = {
+    inherit 
+      inputs
+      homeProfile
+    ;
+  };
+
+  modules = [
+    ../home-manager
+  ];
+}
+
+# inputs.nixpkgs.lib.nixosSystem {
+#   specialArgs = {
+#     inherit
+#       inputs
+#       lib
+#       hostProfile
+#       flakeRoot
+#       self
+#       hostname
+#       ;
+#   };
+#
+#   modules = [
+#     (flakeRoot + "/hosts")
+#   ];
+# }
