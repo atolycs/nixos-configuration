34cb3b4f0b60e813a59f3fac5da4d5bcb0542a8f
Atolycs <111569596+atolycs@users.noreply.github.com>
2025-04-18 11:57:51 +0900

    feat: add template file ignore on mapHosts function

--- lib/mapHosts.nix
+++ lib/mapHosts.nix
@@ -1,7 +1,7 @@
 { ... }@args:
 with args;
 let
-  hostDirs = builtins.filter (x: x != "default.nix") (
+  hostDirs = builtins.filter (x: x != "default.nix" && x != "test-template") (
     builtins.attrNames (builtins.readDir "/${flakeRoot}/hosts")
   );
   dynamicLists = hostDirs;
