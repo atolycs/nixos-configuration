fd4c5a8196a3905af97a60223e7b07109a055d43
Atolycs <111569596+atolycs@users.noreply.github.com>
2025-04-22 17:05:05 +0900

    feat: getVirtual

---
+++ lib/getVirtual.nix
@@ -0,0 +1,42 @@
+# {self, ...}:
+# let
+#   pkgs = self.inputs.nixpkgs.legacyPackages.${builtins.currentSystem};
+#
+#   detectVirt = builtins.trace "Detecting virtualization type..." (
+#     builtins.readFile (
+#       pkgs.writeScript "detect-virt.sh" ''
+#         #!/bin/bash
+#         ${pkgs.systemd}/bin/systemd-detect-virt || echo "none"
+#       ''
+#     )
+#   );
+# in {
+#   config = {
+#     virtualizationType = detectVirt;
+#   };
+# }
+
+{self, ...}:
+let
+  pkgs = self.inputs.nixpkgs.legacyPackages.${builtins.currentSystem};
+
+  detectVirtScript = pkgs.writeScript "detect-virt.sh" ''
+  #!/bin/bash
+    ${pkgs.systemd}/bin/systemd-detect-virt || echo "none"
+  '';
+
+  drv = derivation {
+      name = "getVirtual";
+      builder = "/bin/sh";
+      args = [ "-c" "${pkgs.systemd}/bin/systemd-detect-virt || echo 'none' > $out"];
+      system = builtins.currentSystem;
+      # buildCommand = ''
+      #   mkdir -p $out
+      #   result=$(${detectVirtScript})
+      #   echo $result >> $out/result
+      # '';
+  };
+in "${builtins.readFile drv}"
+
+# in self.inputs.nixpkgs.legacyPackages.${builtins.currentSystem}.stdenv.mkDerivation {
+# }
