06bb0d853d131dd5c28a023ecc5b3d7dfbc7de0d
Atolycs <111569596+atolycs@users.noreply.github.com>
2025-05-15 10:16:04 +0900

    feat: user config function

---
+++ features/nixos/users/default.nix
@@ -0,0 +1,30 @@
+# https://github.com/thexyno/nixos-config/blob/02188142e6795ecab889cd3c5419b958b26c9a96/nixos-modules/user/default.nix
+{ config, lib, pkgs, ...}:
+let
+  cfg = config.atlConfig.user;
+  username = cfg.username;
+  extraGroups = cfg.extraGroups;
+in {
+  options.atlConfig.user = {
+    enable = lib.mkEnableOption "Enable My User";
+    username = lib.mkOption {
+      type = lib.types.str;
+      default = "atolycs";
+      description = "My username for this system";
+    };
+
+    extraGroups = lib.mkOption {
+      type = lib.types.listOf lib.types.str;
+      default = [];
+    };
+
+  };
+
+  config = lib.mkIf cfg.enable {
+    programs.bash.enable = true;
+    users.extraUsers.${username} = {
+      isNormalUser = true;
+      extraGroups = ["wheel" "cdrom"] ++ extraGroups;
+    };
+  };
+}
