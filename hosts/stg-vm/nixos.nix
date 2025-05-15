65695b1f95cf84f126b4159af7f4a05aa72d3cf3
Atolycs <111569596+atolycs@users.noreply.github.com>
2025-05-15 09:50:40 +0900

    feat: add atlas (stg-vm)

---
+++ hosts/stg-vm/nixos.nix
@@ -0,0 +1,31 @@
+# this is test template
+{ self, ... }:
+with self;
+{
+  imports = [
+    self.nixosModules.boot.systemd-boot
+    self.nixosPresets.kvm
+    self.nixosModules.boot.systemd-boot
+    self.nixosModules.desktop-profiles.gdm
+    ./mountPoint
+  ];
+
+  networking.hostName = "atlas";
+  # atlConfig.bindfs =
+  #   {
+  #     enable = true;
+  #   mounts = {
+  #   "/home/atolycs/test" = {
+  #     mountFrom = "/test";
+  #     fsType = "tmpfs";
+  #   };
+  #   "/home/atolycs/test2" = {
+  #     mountFrom = "/test2";
+  #     fsType = "tmpfs";
+  #   };
+  #  };
+  #   };
+
+  locale.sortfix = true;
+  nixpkgs.hostPlatform = "x86_64-linux";
+}
