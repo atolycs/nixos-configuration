ce6c35a8087ca662d8d5e74ce2b5f1a3b3b11a20
Atolycs <111569596+atolycs@users.noreply.github.com>
2025-05-19 11:11:42 +0900

    feat: gdm

--- home-manager/atolycs@stg-vm/home.nix
+++ home-manager/atolycs@stg-vm/home.nix
@@ -1,7 +1,35 @@
 {
+  home-manager,
   self,
+  pkgs,
   ...
 }:
 {
+  imports = [
+    self.homeModules.desktop-manager.gdm
+  ];
 
+  desktop-manager.gdm = {
+    packages = with pkgs; [
+      gnomeExtensions.open-bar
+    ];
+
+    enable-plugins = with pkgs; [
+      gnomeExtensions.open-bar.extensionUuid
+    ];
+
+    dconf = {
+      "org/gnome/shell/extensions/openbar" = {
+        accent-color = [
+          '0.000'
+          '0.750'
+          '0.750'
+        ];
+        bartype = "Trilands";
+        reloadstyle = true;
+      };
+    };
+  };
+
+  systemd.user.startServices = "sd"
 }
